b5d6b92ab71a693c745b61553b59070fadfc72a8
rutaitp <rk2906@nyu.edu>
2018-04-18 16:28:10 -0400

    remove from cart

--- client/store/cart.js
+++ client/store/cart.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 //action types
 const GET_CART = 'GET_CART';
 const ADD_TO_CART = 'ADD_TO_CART';
+const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+//forth when we want to remove cart all together
 
 //action creators
 const getCart = cart => ({
@@ -13,7 +15,12 @@ const getCart = cart => ({
 const addToCart = product => ({
   type: ADD_TO_CART,
   product
-})
+});
+
+const removeFromCart = product => ({
+  type: REMOVE_FROM_CART,
+  product
+});
 
 //get single cart thunk
 export const fetchSingleCart = (id) => dispatch =>
@@ -26,16 +33,35 @@ export const fetchSingleCart = (id) => dispatch =>
   .catch(err => console.log(err));
 
 //add to cart thunk
-// export const postToCart = (productId) => dispatch =>
-//   axios.post(`/api/cart/${}`)
+export const postToCart = (id, productId) => dispatch =>
+  axios.post(`/api/cart/${id}`, productId)
+  .then(res => res.data)
+  .then(newProductId => {
+    const action = addToCart(newProductId)
+    dispatch(action);
+  })
+  .catch(err => console.log(err));
+
+//remove from cart thunk
+export const deleteFromCart = (id, productId) => dispatch =>
+  axios.delete(`/api/cart/${id}`)
+  .then(() => {
+    const action = removeFromCart(productId);
+    dispatch(action);
+  })
+  .catch(err => console.log(err));
 
 //reducer
-const singleCart = [];
+const defaultCart = [];
 
-export default function(state = singleCart, action){
+export default function(state = defaultCart, action){
   switch (action.type){
     case GET_CART:
       return action.cart;
+    case ADD_TO_CART:
+      return [...state, action.product];
+    case REMOVE_FROM_CART:
+      return state.filter(product => product !== action.product)
     default: return state;
   }
 }
