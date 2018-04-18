5381a17c0f5346aab5413bfd6901c80b683e872d
Edison Alulema <edisonalulema@Edisons-MacBook-Air.local>
2018-04-18 17:05:06 -0400

    spacebar

---
+++ client/components/cart.js
@@ -0,0 +1,38 @@
+// import React from 'react'
+// import PropTypes from 'prop-types'
+// import {connect} from 'react-redux'
+// import {NavLink} from 'react-router-dom'
+
+
+// export class Cart extends Component {
+
+
+//   ComponentDidMount() {
+//   this.props.fetchData();
+//   }
+
+
+
+//   render() {
+
+
+//     return(
+//       <div>
+//       </div>
+//     )
+//   }
+
+
+// }
+
+// const mapDispatch = (dispatch, ownProps) => (
+//   {
+//     fetchData: () => {
+//       const id
+//     }
+//   }
+// )
+
+
+
+// export default connect(mapState, mapDispatch)(Cart);
